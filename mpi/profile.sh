a072c1f3edcc4b3cfd413715f29fa7e89f32deb4
Daniele De Sensi <ddesensi@einstein>
2023-03-29 10:23:46 +0200

    [ADD] Profiling script.

---
+++ mpi/profile.sh
@@ -0,0 +1,7 @@
+NODES=32
+rm -f gmon.out-*
+export GMON_OUT_PREFIX=gmon.out-
+mpirun -x GMON_OUT_PREFIX -np ${NODES} ./test_profile &> log.txt
+gprof -s ./test_profile gmon.out-*
+gprof ./test_profile gmon.sum > profile.out
+rm -f gmon.out-* gmon.sum
