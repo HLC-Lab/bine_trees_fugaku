061c6c16bd1cff434fc2632bab7569cc802c8e57
Daniele De Sensi <d.desensi.software@gmail.com>
2025-01-31 12:24:24 +0100

    [IMP] Simplified remapping.

--- simulation/binomial.py
+++ simulation/binomial.py
@@ -2,7 +2,11 @@
 import math
 from itertools import product
 
-distances = [1, 1, 3, 5, 11, 21, 43, 85, 171, 341]
+distances = [1, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461, 10923, 21845, 43691, 87381, 174763, 349525]
+# smallest_negabinaries[i] contains the smallest number that can be represented with a negabinary of i bits
+smallest_negabinaries = [0, 0, -2, -2, -10, -10, -42, -42, -170, -170, -682, -682, -2730, -2730, -10922, -10922, -43690, -43690, -174762, -174762]
+# largest_negabinaries[i] contains the largest number that can be represented with a negabinary of i bits
+largest_negabinaries = [0, 1, 1, 5, 5, 21, 21, 85, 85, 341, 341, 1365, 1365, 5461, 5461, 21845, 21845, 87381, 87381, 349525]
 
 # Gets the distance of the peer of rank 'sender' at the step-th step
 def get_peer_distance(sender, step, num_ranks, collective):
@@ -76,7 +80,42 @@ def decimal_to_negabinary(decimal):
     # Negabinary digits are collected in reverse order
     return ''.join(reversed(negabinary))
 
+# For non-powers of 2, a rank might have two valid representations,
+# one of which has a one as the MSB (indicating that the node is reached in the last step -- i.e., that it is a leaf of the binomial tree).
+# I should always consider the representation that does not have a 1 as the MSB, since the only ranks
+# that have a 1 as the MSB are those that can **ONLY** be reached in the last step.
 def get_rank_negabinary(q, num_nodes):
+    # Generates the negabinary both for q  and for  q - num_nodes (if q is odd)
+    # Generates the negabinary both for -q and for -q + num_nodes (if q is even)
+    num_bits = int(math.ceil(math.log2(num_nodes)))
+    
+    nba = None
+    nbb = None    
+
+    if q % 2:
+        if smallest_negabinaries[num_bits] <= q <= largest_negabinaries[num_bits]:
+            nba = decimal_to_negabinary(q).zfill(num_bits)
+        if smallest_negabinaries[num_bits] <= q - num_nodes <= largest_negabinaries[num_bits]:
+            nbb = decimal_to_negabinary(q - num_nodes).zfill(num_bits)
+    else:
+        if smallest_negabinaries[num_bits] <= -q <= largest_negabinaries[num_bits]:
+            nba = decimal_to_negabinary(-q).zfill(num_bits)
+        if smallest_negabinaries[num_bits] <= -q + num_nodes <= largest_negabinaries[num_bits]:
+            nbb = decimal_to_negabinary(-q + num_nodes).zfill(num_bits)
+
+    assert(nba is not None or nbb is not None)
+
+    if nba is None and nbb is not None:
+        return nbb
+    elif nba is not None and nbb is None:
+        return nba
+    else: # Check MSB
+        if nba[0] == "1":
+            return nba
+        else:
+            return nbb
+
+def get_rank_negabinary_old(q, num_nodes):
     # Generates the negabinary both for q  and for  q - num_nodes (if q is odd)
     # Generates the negabinary both for -q and for -q + num_nodes (if q is even)
     if q % 2:
@@ -100,10 +139,19 @@ def get_rank_negabinary(q, num_nodes):
     nb = nb.zfill(int(math.log2(num_nodes)))
 
     return nb
+
+def int_to_bin(n, num_bits):
+    return bin(n)[2:].zfill(num_bits)
     
 def get_steps_list(q, num_nodes):
     # Get the negabinary representation of the rank
     nb = get_rank_negabinary(q, num_nodes)
+    bit_indexes = int_to_bin(int(nb, 2) ^ (int(nb, 2) >> 1), int(math.log2(num_nodes)))
+    # Revert the bit indexes
+    bit_indexes = bit_indexes[::-1]
+    # Return the indexes of the steps
+    return [i for i, bit in enumerate(bit_indexes) if bit == '1']
+    ''' 
     # Now scan the negabinary (backwards -- from LSB) to get the sequence of steps
     steps = []
     for i in range(len(nb)):
@@ -115,6 +163,14 @@ def get_steps_list(q, num_nodes):
             if pos > 0 and nb[pos - 1] == '1': # If the previous bit is 1 or if we reached the MSB
                 steps.append(i)
     return steps
+    '''
+
+def get_remapped_rank(q, num_nodes):
+    # Get the negabinary representation of the rank
+    nb = get_rank_negabinary(q, num_nodes)
+    bit_indexes = int_to_bin(int(nb, 2) ^ (int(nb, 2) >> 1), int(math.log2(num_nodes)))
+    # Revert the bit indexes and convert back to int
+    return int(bit_indexes[::-1], 2)
 
 # Build the tree for the swing-flat algorithm
 # @param num_nodes: number of nodes participating in the collective
@@ -156,19 +212,7 @@ def build_tree_swing_flat(num_nodes, root, step, collective, children, parent, r
         # Generate the negabinary representation for each rank
         relabels[0] = 0
         for q in range(1, num_nodes):
-            steps = get_steps_list(q, num_nodes)
-            
-            binary = [0]*int(math.log2(num_nodes))
-            for s in steps:
-                binary[s] = 1
-            
-            # Convert binary to decimal
-            decimal = 0
-            for i in range(len(binary)):
-                decimal += binary[i]*2**i
-            
-            #print("Remapping rank {} to {} (nb: {}) (steps: {})".format(q, decimal, nb, steps))
-            relabels[q] = decimal
+            relabels[q] = get_remapped_rank(q, num_nodes)
     
     #print(relabels)
     assert(sorted(relabels) == list(range(num_nodes)))
@@ -262,9 +306,25 @@ def get_children_range(children, relabels, num_nodes, r):
 # @param root: rank of the root of the collective
 # @param rank: the rank for which we want to get the tx info (for multi-root collectives, it is the same as root)
 def get_tx_info_swing_flat_contiguous(num_nodes, collective, root, rank=-1):
-    assert(is_power_of_two(num_nodes))
+    if collective == "ALLREDUCE" or collective == "ALLGATHER" or collective == "REDUCE-SCATTER":
+        assert(is_power_of_two(num_nodes))
     if rank == -1:
         rank = root
+    
+    # For the BCAST, each node starts when it is reached by the data 
+    # coming from the root
+    gap = 0
+    if (collective == "BCAST" or collective == "SCATTER"):
+        # Remap the tree. I.e., the root is always 0
+        if root % 2 == 0:
+            gap = root
+            root = 0
+            rank -= gap
+        else:
+            gap = root - 1
+            root = 1
+            rank -= gap
+    
     # Build the binomial tree (loopless)
     children = [None]*num_nodes
     parent = [None]*num_nodes        
@@ -276,6 +336,7 @@ def get_tx_info_swing_flat_contiguous(num_nodes, collective, root, rank=-1):
     # For the BCAST, each node starts when it is reached by the data 
     # coming from the root
     if (collective == "BCAST" or collective == "SCATTER") and rank != root:
+        # Find the step from which I should start to send data
         #steps = get_steps_list(rank, num_nodes)
         #starting_step = sorted(steps)[-1] + 1
         nb = get_rank_negabinary(rank, num_nodes)
@@ -283,7 +344,11 @@ def get_tx_info_swing_flat_contiguous(num_nodes, collective, root, rank=-1):
         # That's the step in which I am going to be reached by rank 0 data.
         # Since index 0 would be the most significant bit, we reverse the representation
         # so that indexes are consistent with the bit position (i.e., 0 is the rightmost)
-        starting_step = nb[::-1].rfind('1') + 1 # +1 because I want to start from the next step (on the first step I only receive)        
+
+        # +1 because I want to start from the next step (on the first step I only receive)        
+        # I do the max with 0 because when a 1 is not found (e.g., for rank 0 representation),
+        # the rfind returns -1, but in practice I want to start from step 0.
+        starting_step = max(nb[::-1].rfind('1'), 0) + 1 
 
     #print(starting_step)
     # Now use the two pieces of info computed above to build the tx info
@@ -296,6 +361,7 @@ def get_tx_info_swing_flat_contiguous(num_nodes, collective, root, rank=-1):
             # Prepare scheduling info according to collectives
             if collective == "BCAST":
                 peer = get_peer(rank, s, num_nodes, collective)
+                peer = (peer + gap) % num_nodes
                 print("Step {}: Peer: {} Blocks: (0-{})".format(s, peer, num_nodes - 1))
             elif collective == "ALLREDUCE" or collective == "REDUCE-SCATTER" or collective == "ALLGATHER" or collective == "SCATTER":
                 min_block_id_s = min_block_id_r
@@ -316,4 +382,4 @@ def get_tx_info_swing_flat_contiguous(num_nodes, collective, root, rank=-1):
                 print("Step {}: Peer: {} Blocks to send: ({}-{})".format(s, peer, min_block_id_s, max_block_id_s))
 
 validate_all()
-get_tx_info_swing_flat_contiguous(8, "BCAST", 0, 3)
+get_tx_info_swing_flat_contiguous(6, "BCAST", 0, 5)
