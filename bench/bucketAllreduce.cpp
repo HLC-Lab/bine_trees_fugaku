ca22c8923b607f287d63d8cc4ab905f49c59b2ba
SBA0486 <140486157+SBA0486@users.noreply.github.com>
2025-04-08 15:31:55 +0900

    Bucket algo removed non necessary functions

--- bench/bucketAllreduce.cpp
+++ bench/bucketAllreduce.cpp
@@ -6,7 +6,7 @@
 AllreduceRSAG implementation from:
 https://github.com/andrea-garritano/allreduce/blob/master/allreduce.cpp
 
-RingAllreduce from mpich and baidu github:
+AllreduceRing from mpich and baidu github:
 https://github.com/pmodels/mpich/blob/bb7f0a9f61dbee66c67073f9c68fa28b6f443e0a/src/mpi/coll/allreduce
 https://github.com/baidu-research/baidu-allreduce/blob/master/collectives.cu
 
@@ -24,31 +24,12 @@ https://github.com/HLC-Lab/swing-allreduce-sim/blob/98ad4ebd58b54d4de55291d9d8fc
 #include<vector>
 #include <cassert>
 
+#include <stdio.h>
+#include <mpi-ext.h>              // Include header file
+#define LDIM 3
+#define TDIM 6
+
 using namespace std;
-
-// Allocate a new memory buffer on CPU or GPU.
-double* alloc(size_t size) {
-    return new double[size];
-}
-// Deallocate an allocated memory buffer.
-void dealloc(double* buffer) {
-    delete[] buffer;
-}
-
-// Copy data from one memory buffer to another on CPU or GPU.
-// Both buffers must resize on the same device.
-void copy(double* dst, double* src, size_t size) {
-    std::memcpy((void*) dst, (void*) src, size * sizeof(double));
-}
-
-// Copy data from one memory buffer to another on CPU or GPU.
-// Both buffers must resize on the same device.
-void reduce(double* dst, double* src, size_t size) {
-    // Accumulate values from `src` into `dst` on the CPU.
-    for(size_t i = 0; i < size; i++) {
-        dst[i] += src[i];
-    }
-}
 
 void reduce_local(const void* inbuf, void* inoutbuf, int count, MPI_Datatype datatype, MPI_Op op) {
     if(datatype == MPI_INT32_T){
@@ -118,247 +99,6 @@ void reduce_local(const void* inbuf, void* inoutbuf, int count, MPI_Datatype dat
     }
 }
 
-// Collect the input buffer sizes from all ranks using standard MPI collectives.
-// These collectives are not as efficient as the ring collectives, but they
-// transmit a very small amount of data, so that is OK.
-    std::vector<size_t> AllgatherInputLengths(int size, size_t this_rank_length) {
-    std::vector<size_t> lengths(size);
-    MPI_Allgather(&this_rank_length, 1, MPI_UNSIGNED_LONG,
-                  &lengths[0], 1, MPI_UNSIGNED_LONG, MPI_COMM_WORLD);
-    return lengths;
-}
-
-/**
- * Wrapper function around MPI_Allreduce (leave this unchanged)
- * @param sendbuf Send buffer containing count doubles (input)
- * @param recvbuf Pre-allocated receive buffer (output)
- * @param count Number of elements in the send and receive buffers
- */
-void allreduce(double *sendbuf, double *recvbuf, int count)
-{
-	MPI_Allreduce(sendbuf, recvbuf, count, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-}
-
-/**
- * Wrapper function around MPI_Allreduce (implement reduce-scatter / allgather algorithm)
- * @param sendbuf Send buffer containing count doubles (input)
- * @param recvbuf Pre-allocated receive buffer (output)
- * @param count Number of elements in the send and receive buffers
- */
-void allreduceRSAG(double *sendbuf, double *recvbuf, int count)
-{
-	// Note: assume MPI_DOUBLE, MPI_SUM and MPI_COMM_WORLD parameters
-	// These may be hard-coded in the implementation. You may assume
-	// that the number of processes is a power of two and you may assume
-	// that "count" is a multiple of P.
-
-	// Implement the allreduce algorithm using only point-to-point
-	// communications, using the reduce-scatter / allgather algorithm
-	// (see exercises slides for communication scheme)
-
-	// Do not change the function signature as your implementation
-	// will be automatically validated using a test framework.
-
-	// Do not overwrite the sendbuf contents. Allocate a temp buffer
-	// if necessary.
-
-	int thisProc, nProc;
-    MPI_Comm_rank( MPI_COMM_WORLD, &thisProc );
-    MPI_Comm_size( MPI_COMM_WORLD, &nProc );
-    MPI_Status status;
-
-    int dest;
-    int width;
-    double *myData = new double[count];
-    for (int i=0; i<count; i++)
-        myData[i]=sendbuf[i];
-
-    double *temp_sendbuf = new double[count];
-    for (int i=0; i<count; i++)
-        temp_sendbuf[i]=sendbuf[i];
-
-    int nPhases = log2(nProc);
-
-    for (int p = 0; p<nPhases; p++){
-        width = (count/pow(2, p+1));
-
-        if (thisProc%((int)pow(2, p+1))<(int)pow(2, p+1)/2){
-            dest = thisProc + pow(2,p);
-            for (int i=0; i<width; i++)
-                temp_sendbuf[i]=myData[i+width]; //second half of the message
-            MPI_Send(temp_sendbuf, width, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-            MPI_Recv(recvbuf, width, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD, &status);
-
-            for (size_t i = 0; i < width; i++)
-                myData[i] += recvbuf[i]; //sum the replay
-        }   
-        else{
-            dest = thisProc - pow(2,p);
-
-            MPI_Recv(recvbuf, width, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD, &status);
-
-            
-            for (int i=0; i<width; i++)
-                temp_sendbuf[i]=myData[i]; //first half of the message
-
-            for (size_t i = 0; i < width; i++)
-                myData[i+width] += recvbuf[i]; //sum the replay
-            MPI_Send(temp_sendbuf, width, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-
-            //Copy the second half on the first half
-            for (size_t i = 0; i < width; i++)
-                myData[i]=myData[i+width];
-        }
-    }
-    for (int p = nPhases-1; p>=0; p--){
-        width = (count/pow(2, p+1));
-        if (thisProc%((int)pow(2, p+1))<(int)pow(2, p+1)/2){
-            dest = thisProc + pow(2,p);
-            for (int i=0; i<width; i++)
-                temp_sendbuf[i]=myData[i];
-            MPI_Send(temp_sendbuf, width, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD);
-            MPI_Recv(recvbuf, width, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD, &status);
-
-            for (size_t i = 0; i < width; i++)
-                myData[i+width] = recvbuf[i];
-        }   
-        else{
-            dest = thisProc - pow(2,p);
-
-            MPI_Recv(recvbuf, width, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD, &status);
-            
-            for (int i=0; i<width; i++)
-                temp_sendbuf[i]=myData[i];
-
-            
-            for (size_t i = 0; i < width; i++)
-                myData[i+width] = myData[i];
-
-            for (size_t i = 0; i < width; i++)
-                myData[i] = recvbuf[i];
-            MPI_Send(temp_sendbuf, width, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-
-        }
-    }
-
-    for (size_t i = 0; i < count; i++)
-        recvbuf[i] = myData[i];
-
-    delete [] myData;
-    delete [] temp_sendbuf;
-
-}
-
-
-/* Routine to schedule a ring exchange based allreduce. The algorithm is
- * based on Baidu's ring based allreduce. http://andrew.gibiansky.com/ */
-//void RingAllreduce(float* data, size_t length, float** output_ptr)
-//void allreduceRing(double *sendbuf, double *recvbuf, int count)
-void allreduceRing(double* data, double* output, int count)
-{
-    // Get MPI size and rank.
-    int rank, nProc;
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &nProc );
-    
-    // Partition the elements of the array into N approximately equal-sized
-    // chunks, where N is the MPI size.
-    const int segment_size = count / nProc;
-    std::vector<size_t> segment_sizes(nProc, segment_size);
-
-    const size_t residual = count % nProc;
-    for (size_t i = 0; i < residual; ++i) {
-        segment_sizes[i]++;
-    }
-
-    // Compute where each chunk ends.
-    std::vector<size_t> segment_ends(nProc);
-    segment_ends[0] = segment_sizes[0];
-    for (size_t i = 1; i < segment_ends.size(); ++i) {
-        segment_ends[i] = segment_sizes[i] + segment_ends[i - 1];
-    }
-
-    // The last segment should end at the very end of the buffer.
-    assert(segment_ends[nProc - 1] == count);
-
-    // Allocate the output buffer.
- //   double* output = new double[count]; // alloc(count);
- //   output_ptr =  output;
-
-    // Copy your data to the output buffer to avoid modifying the input buffer.
-   // copy(output, data, count);
-    std::memcpy((void*) output, (void*) data, count * sizeof(double));
-
-    // Allocate a temporary buffer to store incoming data.
-    // We know that segment_sizes[0] is going to be the largest buffer size,
-    // because if there are any overflow elements at least one will be added to
-    // the first segment.
-    double* buffer = alloc(segment_sizes[0]);
-
-    // Receive from your left neighbor with wrap-around.
-    const size_t recv_from = (rank - 1 + nProc) % nProc;
-
-    // Send to your right neighbor with wrap-around.
-    const size_t send_to = (rank + 1) % nProc;
-
-    MPI_Status recv_status;
-    MPI_Request recv_req;
-    MPI_Datatype datatype = MPI_DOUBLE;
-
-    // Now start ring. At every step, for every rank, we iterate through
-    // segments with wraparound and send and recv from our neighbors and reduce
-    // locally. At the i'th iteration, sends segment (rank - i) and receives
-    // segment (rank - i - 1).
-    for (int i = 0; i < nProc - 1; i++) {
-        int recv_chunk = (rank - i - 1 + nProc) % nProc;
-        int send_chunk = (rank - i + nProc) % nProc;
-        double* segment_send = &(output[segment_ends[send_chunk] -
-                                   segment_sizes[send_chunk]]);
-
-        MPI_Irecv(buffer, segment_sizes[recv_chunk],
-                datatype, recv_from, 0, MPI_COMM_WORLD, &recv_req);
-
-        MPI_Send(segment_send, segment_sizes[send_chunk],
-                MPI_DOUBLE, send_to, 0, MPI_COMM_WORLD);
-
-        double *segment_update = &(output[segment_ends[recv_chunk] -
-                                         segment_sizes[recv_chunk]]);
-
-        // Wait for recv to complete before reduction
-        MPI_Wait(&recv_req, &recv_status);
-
-        reduce(segment_update, buffer, segment_sizes[recv_chunk]);
-    }
-
-    // Now start pipelined ring allgather. At every step, for every rank, we
-    // iterate through segments with wraparound and send and recv from our
-    // neighbors. At the i'th iteration, rank r, sends segment (rank + 1 - i)
-    // and receives segment (rank - i).
-    for (size_t i = 0; i < size_t(nProc - 1); ++i) {
-        int send_chunk = (rank - i + 1 + nProc) % nProc;
-        int recv_chunk = (rank - i + nProc) % nProc;
-        // Segment to send - at every iteration we send segment (r+1-i)
-        double* segment_send = &(output[segment_ends[send_chunk] -
-                                       segment_sizes[send_chunk]]);
-
-        // Segment to recv - at every iteration we receive segment (r-i)
-        double* segment_recv = &(output[segment_ends[recv_chunk] -
-                                       segment_sizes[recv_chunk]]);
-        MPI_Sendrecv(segment_send, segment_sizes[send_chunk],
-                datatype, send_to, 0, segment_recv,
-                segment_sizes[recv_chunk], datatype, recv_from,
-                0, MPI_COMM_WORLD, &recv_status);
-    }
-
-    // Free temporary memory.
-    dealloc(buffer);
-}
-
-#include <stdio.h>
-#include <mpi-ext.h>              // Include header file
-#define LDIM 3
-#define TDIM 6
-
 /***
 // Convert a rank id into a list of d-dimensional coordinates
 static void getCoordFromId(int id, int* dimensions, int dimensions_num, int* coord, int m_p){
@@ -426,7 +166,7 @@ void ringRedScatAG(double* data, int count, int nProc, int rank, int recvfrom, i
     // We know that segment_sizes[0] is going to be the largest buffer size,
     // because if there are any overflow elements at least one will be added to
     // the first segment.
-    double* buffer = alloc(segment_sizes[0]);
+    double* buffer = new double[segment_sizes[0]];
 
     // Recv_from/send_to on each dimention is defined for each node before hand.
     const size_t recv_from = recvfrom;      // (rank - 1 + nProc) % nProc;  
@@ -661,25 +401,15 @@ int main(int argc, char* argv[])
     double *recvbuf_b = new double[count];
     
  // Ref. allreduce implementations 
-    allreduce(sendbuf, recvbufGround, count);    // Default MPI_allreduce
+    // allreduce(sendbuf, recvbufGround, count);    // Default MPI_allreduce
+    MPI_Allreduce(sendbuf, recvbufGround, count, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     // allreduceRSAG(sendbuf, recvbuf, nProc);  // Reduce-scatter/allgather 
-    allreduceRing(sendbuf, recvbuf, count);      // Topology un-aware Ring_allreduce  
+//    allreduceRing(sendbuf, recvbuf, count);      // Topology un-aware Ring_allreduce  
 
-    bool test = true;
-    for (int i=0; i<count; i++){
-//        printf("%f\n", recvbufGround[i]);
-        if (recvbuf[i]!=recvbufGround[i])             test=false;
-    }
-    if (thisProc == 0){
-        if (test)
-            cout<<"Ring Allreduce Test passed"<<endl;
-        else
-            cout<<"Ring Allreduce Test failed"<<endl;
-    }
 
  //  Test bucket allgorithm
     buckAllreduce(sendbuf, recvbuf_b, count);
-    test = true;
+    int test = true;
     for (int i=0; i<count; i++){
  //       printf("reduced value:%f\t, error margin %f\n", recvbuf_b[i], recvbufGround[i] - recvbuf_b[i]);
         if (recvbufGround[i]!=recvbuf_b[i]) test=false ;
